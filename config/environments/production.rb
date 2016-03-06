bc882d05dcec6ef6e37deb8cfc65a84ea28d6577
Harsheek Thanki <harsheekthanki@gmail.com>
2016-03-06 22:48:32 +0000

    Add devise gem to app

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,5 +1,7 @@
 Grammable::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.action_mailer.default_url_options = { host: 'grammable-harsheek-thanki.herokuapp.com'}
+
 
   # Code is not reloaded between requests.
   config.cache_classes = true
