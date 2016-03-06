83502d2da8828e6be5d2ee57ab543aa55fc776c2
Harsheek Thanki <harsheekthanki@gmail.com>
2016-03-06 22:23:54 +0000

    Add create action to grams controller

--- spec/controllers/grams_controller_spec.rb
+++ spec/controllers/grams_controller_spec.rb
@@ -16,7 +16,7 @@ RSpec.describe GramsController, type: :controller do
   end
 
   describe "grams#create action" do
-    it "should successfully create a grame in our database" do
+    it "should successfully create a gram in our database" do
       post :create, gram: {message: 'Hello!'}
       expect(response).to redirect_to root_path
 
