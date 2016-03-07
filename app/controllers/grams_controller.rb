c8c85cdaeb180b5955f23c9246ec012f495823cb
Harsheek Thanki <harsheekthanki@gmail.com>
2016-03-07 23:19:20 +0000

    Add tests for update action in spec. Add update action in grams controller.

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,18 +1,21 @@
 class GramsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
-  def show
-    @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      render text: 'Not Found :(', status: :not_found
-    end
+  def new
+    @gram = Gram.new
   end
 
   def index
   end
 
-  def new
-    @gram = Gram.new
+  def show
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  end
+
+  def edit
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
   end
 
   def create
@@ -24,7 +27,24 @@ class GramsController < ApplicationController
     end
   end
 
+  def update
+    @gram = Gram.find_by_id(params[:id])
+    return render_not_found if @gram.blank?
+  
+    @gram.update_attributes(gram_params)
+  
+    if @gram.valid?
+      redirect_to root_path
+    else
+      return render :edit, status: :unprocessable_entity
+    end
+  end
+
   private
+
+  def render_not_found
+    render text: 'Not Found :(', status: :not_found
+  end
 
   def gram_params
     params.require(:gram).permit(:message)
