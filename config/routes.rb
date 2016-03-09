ddcb615088996e35654bee7e25f0bcd4567653a2
Harsheek Thanki <harsheekthanki@gmail.com>
2016-03-09 00:10:00 +0000

    add route to route.rb for create action comments controller. Add create action to comments controller. Set up associations in models for user, comments and gram. Add authenticate user for create action to comments controller. Refactor render_not_found method

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Grammable::Application.routes.draw do
-  devise_for :users
-  root "grams#index"
-  resources :grams
-  
+    devise_for :users
+    root "grams#index"
+    resources :grams do
+      resources :comments, only: :create
+    end
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -57,4 +60,3 @@ Grammable::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
--- spec/controllers/comments_controller_spec.rb
+++ spec/controllers/comments_controller_spec.rb
@@ -7,21 +7,21 @@ RSpec.describe CommentsController, type: :controller do
       u = FactoryGirl.create(:user)
       sign_in u
       post :create, gram_id: p.id, comment: { message: 'awesome gram'}
-      expect(repsonse).to redirect_to root_path
+      expect(response).to redirect_to root_path
       expect(p.comments.length).to eq 1
       expect(p.comments.first.message).to eq "awesome gram"
     end
 
     it "should require a user to be logged in to comment on a gram" do
-      P = FactoryGirl.Create(:gram)
-      post :create, gram_id: p.id, comment: { message: 'awesome gram'}
-      expect(repsonse).to redirect_to new_user_session_path
+      p = FactoryGirl.create(:gram)
+      post :create, gram_id: p.id, comment: { message: 'awesome gram' }
+      expect(response).to redirect_to new_user_session_path
     end
 
     it "should return http status code of not found if the gram isn't found" do
       u = FactoryGirl.create(:user)
       sign_in u
-      post :create, gram_id: 'YOLOSWAG', comment: { message: 'awesome gram'}
+      post :create, gram_id: 'YOLOSWAG', comment: {message: 'awesome gram'}
       expect(response).to have_http_status :not_found
     end
   end
